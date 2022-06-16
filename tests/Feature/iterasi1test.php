d8651affcb11ba4619bf53ff918c80def1249524
fajriansyah1127 <64572016+fajriansyah1127@users.noreply.github.com>
2022-06-16 12:06:48 +0800

    unit testing

--- tests/Feature/iterasi1test.php
+++ tests/Feature/iterasi1test.php
@@ -4,21 +4,26 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class iterasi1 extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    use WithFaker;
 
-        $response->assertStatus(200);
+    public function test_Login()
+    {
+         $response = $this->post('/auth', [
+            'email' => 'fajriansyah573@gmail.com',
+            'password' => 'asdfghjkl',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
     }
     // public function test_users_can_authenticate_using_the_login_screen()
     // {
@@ -31,6 +36,111 @@ class iterasi1 extends TestCase
 
     //     $this->assertAuthenticated();
     //     $response->assertRedirect(RouteServiceProvider::HOME);
-    // sarah jelek
+    // 
+    // }  i
+    
+    public function test_tambah_data_user()
+    {
+        Storage::fake('avatars');
+        $file = UploadedFile::fake()->image('avatar.jpg');
+        $user = User::factory()->create();
+        $response = $this->get->post(route('user.store'), [
+            'nama' => $user->nama,
+            'email' => $user->email,
+            'kontak' => $user->kontak,
+            'alamat' => $user->alamat,
+            'jabatan' => $user->jabatan,
+            'role' => $user->role,
+            'foto' => $file,
+            'password' =>$user->password,
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_lihat_data_user()
+    {
+        // $user= ([
+        //              'email' => 'fajriansyah573@gmail.com',
+        //              'password' => 'asdfghjkl',
+        //        ]);
+        // $user = User::factory()->create();
+        // $response = $this->actingAs($user)
+        //     ->get(route('user.index'));
+        $response = $this->get(route('user.show', [ 'user' => 1, ]));
+        $response->assertStatus(302);
+    }
+
+    public function test_lihat_user()
+    {
+        $response = $this->get(route('user.index'));
+        $response->assertStatus(302);
+    }
+
+    public function test_edit_data_user()
+    {
+        Storage::fake('avatars');
+        $file = UploadedFile::fake()->image('avatar.png');
+        $user = User::factory()->create();
+        $response = $this->get->put(route('user.update'), [
+            'nama' => $user->nama,
+            'email' => $user->email,
+            'kontak' => $user->kontak,
+            'alamat' => $user->alamat,
+            'jabatan' => $user->jabatan,
+            'role' => $user->role,
+            'foto' => $file,
+            'password' =>$user->password,
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_hapus_data_user()
+    {
+        $response = $this->get(route('user.destroy', [ 'user' => 19, ]));
+        $response->assertStatus(302);
+    }
+
+    public function test_lihat_profil()
+    {
+        $response = $this->get(route('profile.index', [ 'user' => 1, ]));
+        $response->assertStatus(302);
+    }
+
+    public function test_edit_profile()
+    {
+        $response = $this->get(route('profile.update', [ 
+            'user' => 3,
+            'password' => 'baruini', 
+        
+        ]));
+        $response->assertStatus(302);
+    }
+
+    // public function test_reset_password()
+    // {
+    //     $response = $this->get(route('profile.index', [ 
+    //         'user' => 1,
+    //         'password' => 'baruini', 
+        
+    //     ]));
+    //     $response->assertStatus(302);
     // }
+
+    // public function test_hapus_hak_cipta()
+    // {
+    
+    //     $HakCipta = User::factory()->create();
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user)
+    //         ->delete(route('HakCipta.destroy', $HakCipta->id));
+    //     $response->assertStatus(302);
+
+    //     $this->get('/login')
+    //          ->see('Laravel')
+    //          ->dontSee('Rails');
+
+    // }
+
 }
